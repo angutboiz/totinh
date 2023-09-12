eed294e0d396c2436e93f6f2419d8a47cff15561
Trọng An <96829540+angutboiz@users.noreply.github.com>
2023-09-12 11:33:28 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/angutboiz";
+            window.location = "https://www.facebook.com/trongandev/";
           },
         });
       }
